f2f1ae69edbfb90ca7345ed96a1444c9e35896e0
Adam Fisher <amgfisher90@gmail.com>
2023-02-23 15:00:06 -0500

    proxy setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>PFS-frontend-reactnative</Text>
+      <Text>PFS Frontend!</Text>
       <StatusBar style="auto" />
     </View>
   );
